9f469c644f7f47a22a9a437f14ff29e0cf8ac81c
Nahuel Cialone <ncialone@gmail.com>
2023-05-19 11:13:10 -0300

    2nda app -items- agregada vista detallada del producto y panel de productos misma categoría

---
+++ item/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+#recordar el nombre de esta app para poder agregarlo a urls del proyecto principal
+app_name = 'item'
+
+urlpatterns = [
+    #el path espera el id del item para desplegar el view de detalles
+    path('<int:pk>/', views.detalle, name='detalle'),
+]
--- item/views.py
+++ item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
-# Create your views here.
+def detalle(request, pk):
+    #se pasa por parametro el id del item a buscar
+    item = get_object_or_404(Item, pk=pk)
+    #traemos 3 items relacionados de la misma categoria, filtrado por "no vendidos"
+    items_relacionados = Item.objects.filter(categoria=item.categoria, vendido=False).exclude(pk=pk)[0:3]
+    
+    return render(request, 'item/detalle.html',{
+        'item': item,
+        'items_relacionados': items_relacionados
+    })
