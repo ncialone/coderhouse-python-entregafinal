6226516012054661178f46098a7ba0aeec4f0baf
Nahuel Cialone <ncialone@gmail.com>
2023-05-19 18:21:41 -0300

    app -core- agregada vista Login

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ingresa tu nombre de usuario',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingresa tu contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
 
 class Registro(UserCreationForm):
     class Meta:
--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,7 @@
+
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from .forms import LoginForm
 from . import views
 #recordar el nombre de esta app para poder agregarlo a urls del proyecto principal
 app_name = 'core'
@@ -7,4 +10,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contacto/', views.contacto, name='contacto'),
     path('registro/', views.registro, name='registro'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
